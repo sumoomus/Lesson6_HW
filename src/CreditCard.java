65a777f95892d5d8c3893f3d01d741f0ef756ede
sumoomus <cummbin0@gmail.com>
2023-04-02 12:19:42 +0300

    HW6

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class CreditCard {
+public class CreditCard {
+    private int accountNumber;
+    private double currentAmount;
+
+    public CreditCard(int accountNumber, double currentAmount) {
+        this.accountNumber = accountNumber;
+        this.currentAmount = currentAmount;
+    }
+
+    public void charge(double amount) {
+        currentAmount += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (currentAmount >= amount) {
+            currentAmount -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    public void display() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Amount: " + currentAmount);
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CreditCard card1 = new CreditCard(123456789, 1000.0);
+        CreditCard card2 = new CreditCard(987654321, 500.0);
+        CreditCard card3 = new CreditCard(456789123, 200.0);
+
+        card1.charge(50.0);
+        card2.charge(100.0);
+        card3.withdraw(50.0);
+
+        card1.display();
+        card2.display();
+        card3.display();
     }
 }
+
